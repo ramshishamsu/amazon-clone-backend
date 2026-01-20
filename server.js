188d4a38dbf7d549f9c2e818b9fd1617ecb5af82
Ramshishamsu <ramshishamsu22@gmail.com>
2026-01-20 11:15:04 +0530

    Fix server running

--- server.js
+++ server.js
@@ -36,3 +36,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
+app.get("/", (req, res) => {
+  res.send("Amazon Clone Backend API is running 🚀");
+});
